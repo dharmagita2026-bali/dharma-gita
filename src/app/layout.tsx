e624e7a8053ab5a86b8a714324be523650025023
Dharma-gita <dharmagita2026@gmail.com>
2026-06-07 19:28:08 +0800

    Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Dharma Gita - Belajar Kidung Bali Tanpa Batasan",
+  description: "Dharma Gita adalah sebuah platform e-learning untuk melestarikan seni kidung Bali melalui teknologi yang mudah diakses semua kalangan.",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
